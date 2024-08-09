bd3cbc35ae976456b6f592ce4b8c0af427caf65d
Shivam Sourav <xxxxyyyy1920@gmail.com>
2024-08-09 15:34:46 +0530

    fix send money

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import {useSearchParams} from "react-router-dom";
 import axios from 'axios'
 import { API_BASE_URL } from '../components/BackLink';
+import { useNavigate } from 'react-router-dom';
 
 export const SendMoney = () => {
     const [searchParams] = useSearchParams();
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setAmount] = useState(0);
+    const navigate = useNavigate();
 
     return (
         <div className='flex justify-center h-screen bg-gray-100'>
@@ -36,9 +38,10 @@ export const SendMoney = () => {
                                     amount
                                 },{
                                     headers: {
-                                        Authorization: "Bearer" + localStorage.getItem("token")
+                                        Authorization: "Bearer " + localStorage.getItem("token")
                                     }
                                 })
+                                navigate("/dashboard");
                             }} className='justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white'>
                                 Initiate Transfer
                             </button>
